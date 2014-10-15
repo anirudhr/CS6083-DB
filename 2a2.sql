2fcb4477a67805d5a86a523da3218603a7359357
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-10-15 19:14:42 -0400

    finished q2a

--- 2a2.sql
+++ 2a2.sql
@@ -1,2 +1,7 @@
 /*(ii) Output the name of the branch that made the most money from rentals in 2009.*/
 USE videoRentalChain;
+SELECT branch.bname FROM branch
+WHERE bid = (SELECT bid FROM analyst
+            GROUP BY bid
+            ORDER BY SUM(tot_income) DESC
+            LIMIT 1)
