d31b84cbdd94ba596bd0482076b8f99cb673eb02
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-10-15 19:31:59 -0400

    Finished 2b

--- 2b2.sql
+++ 2b2.sql
@@ -2,5 +2,3 @@
 (ii) Output the balance of the customer with the name “John Smith”.
 CANNOT BE WRITTEN WITH VIEW. View does not contain balance, so we need to access the "customer" table to retrieve that value.
 */
-USE videoRentalChain;
-SELECT balance 
--- 2b3.sql
+++ 2b3.sql
@@ -2,3 +2,5 @@
 (iii) Add a new customer record for customer “Bob Jones” at “23 Court Street, Brooklyn” with ID 18123.
 */
 USE videoRentalChain;
+INSERT INTO addresses VALUES
+    (18123, "Bob Jones", "23 Court Street, Brooklyn");
