e0efcf62976f24f1f60f1f6e752839ef5d295d27
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-08 16:17:03 -0500

    started q1

---
+++ as3/q1/index.php
@@ -0,0 +1,52 @@
+<html>
+<!--:indentSize=4:tabSize=4:noTabs=true:wrap=soft:-->
+<!--
+Use PHP to implement a web application that supports the following :
+(i) A user enters (via an html form with two text boxes and a “submit” button) a keyword, such as "delicious" or "turkey" and a 10 digit phone number.
+
+(ii) The app initiates a session and stores the phone number as a session variable. The app creates an html form displaying all sandwiches that have the keyword in the description, along with the description of each, the sizes available, and their prices, with some means for the user to select a size of a particular kind of sandwich (e.g radio buttons, check boxes, etc). If no keyword is entered, all of the sandwiches (and their sizes and prices) are displayed.
+(iii) Using the form produced in part (ii), the user can select a sandwich and size. The order table is updated as follows:
+If the user has a pending order of the same size of the same kind of sandwich, the quantity on that order is increased by one and the o_time is changed to the current time.
+Otherwise, a new tuple is inserted in the order table, representing this customer’s order of this sandwich, with the current time as the o_time and quantity equal to 1. Note that to do this, the app will need to fetch the customer's phone number from the session variable.
+
+For full credit, you must use prepared statements.
+Test your application, using the data provided on the course page to populate your database. 
+-->
+<head><title>Sandwich Search</title></head>
+<body>
+<?php
+session_start();
+error_reporting(E_ALL);
+
+function striptext($text) {
+    $text = trim($text);
+    $text = stripslashes($text);
+    $text = htmlspecialchars($text);
+    return $text;
+}
+$keyword = striptext($_POST['keyword']);
+$_SESSION['phone'] = $_POST['phone'];
+
+$host = "localhost";
+$user = "root";
+$pass = "toor";
+$db = "SandwichDelivery";
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql_searchkeyword = "SELECT * FROM sandwich WHERE description LIKE '%" . $keyword . "%';";
+$result = $conn->query($sql_searchkeyword);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["sname"]. " - Description: " . $row["description"]. "<br/>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
+</body>
+</html>
