1dff29e228ecf1b839d06439b8f271a54ca2f1e0
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-09 00:00:50 -0500

    unifying

--- as3/q1/index.php
+++ as3/q1/index.php
@@ -3,6 +3,22 @@
 <title>Sandwich Order Form</title>
 </head>
 <body>
+<?php
+function striptext($text) {
+    $text = trim($text);
+    $text = stripslashes($text);
+    $text = htmlspecialchars($text);
+    return $text;
+}
+$keyword = $phone = $phoneErr = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $keyword = striptext($_POST["keyword"]);
+    $phone = striptext($_POST["phone"]);
+    if (strlen($phone) != 10) {
+        $phoneErr = "Phone number must be 10 digits.";
+    }
+}
+?>
 <h4>Sandwich Order Form</h4>
 <form name="get_cu_info" action="
             <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
@@ -10,26 +26,9 @@
   <span>Keyword</span>
   <input type="text" name="keyword"/>
   <span>Phone</span>
-  <input type="number" name="phone"/>
-  <input type="submit"/>
+  <input type="number" name="phone"/><?php echo $phoneErr; ?>
+  <span><input type="submit"/></span>
 </form>
-<?php
-$keyword = $phone = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $keyword = striptext($_POST["keyword"]);
-    $phone = striptext($_POST["phone"]);
-    echo "POST<br/>";
-}
-else {
-    echo "NOTPOST<br/>";
-}
-function striptext($text) {
-    $text = trim($text);
-    $text = stripslashes($text);
-    $text = htmlspecialchars($text);
-    return $text;
-}
-?>
 </body>
 </html>
 <!--:indentSize=4:tabSize=4:noTabs=true:wrap=soft:-->
