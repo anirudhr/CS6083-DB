76604c4edf4701c9f61be3b1a5f6420d99fa28d8
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-09 16:30:50 -0500

    Works, yay

--- as3/q1/process.php
+++ as3/q1/process.php
@@ -15,7 +15,7 @@ $selection = explode ('|', $_POST["selection"]);
 $sname = $selection[0];
 $size = $selection[1];
 $phone = $_SESSION['phone'];
-
+//echo "Ready to connect! <br/>";
 $host = "localhost";
 $user = "root";
 $pass = "toor";
@@ -24,39 +24,57 @@ $conn = new mysqli($host, $user, $pass, $db);
 
 $chk_pending_query = "SELECT o_time
                         FROM orders
-                        WHERE phone = '$phone'
-                         AND sname = '$sname'
-                         AND size = '$size'
+                        WHERE phone = ?
+                         AND sname = ?
+                         AND size = ?
                          AND status = 'pending'";
 $stmt = $conn->stmt_init();
 if(!$stmt->prepare($chk_pending_query)) {
     echo "Failed to prepare query. <br/> $chk_pending_query <br/>";
 }
 else {
-    $stmt->execute();
+    //echo "Query prepped! <br/>";
+    $stmt->bind_param('sss', $phone, $sname, $size);
+    if (!$stmt->execute()) {
+        echo "Failed to execute query. <br/> $chk_pending_query <br/>";
+    }
     $stmt->bind_result($o_time);
-    $o_time_res = array();
     $len = 0;
     while($stmt->fetch()) {
-        $o_time_res[] = $o_time;
         $len = $len + 1;
     }
-    $o_time = date('Y-m-d H:i:s', time() - 5*60*60); //GMT-5
+    //echo $len . "<br/>";
+    date_default_timezone_set('America/New_York');
+    $o_time = date('Y-m-d H:i:s', time());
     if ($len == 0) {
         //need to insert new tuple
         $insupd_query = "INSERT INTO
-                        orders (phone, sname, size, o_time, quantity, status)
-                        VALUES ('$phone', '$sname', '$size', '$o_time', 1, 'pending')";
+                        orders (o_time, phone, sname, size, quantity, status)
+                        VALUES (?, ?, ?, ?, 1, 'pending')";
     }
     else {
-        //need to update existing tuple. primary key: $phone+$sname+$size
+        //need to update existing tuple(s). composite key: $phone+$sname+$size
         $insupd_query = "UPDATE orders
-                        SET quantity = quantity+1, o_time='$o_time'
-                        WHERE phone='$phone'
-                        AND sname='$sname'
-                        AND size='$size'";
+                        SET quantity = quantity+1, o_time=?
+                        WHERE phone=?
+                        AND sname=?
+                        AND size=?";
     }
-    echo $insupd_query . "<br/>";
+    $stmt=$conn->stmt_init();
+    //echo "Statement initiated! <br/>";
+    if(!$stmt->prepare($insupd_query)) {
+        echo "Failed to prepare query. <br/> $insupd_query <br/>";
+    }
+    else {
+        $stmt->bind_param('ssss', $o_time, $phone, $sname, $size);
+        if (!$stmt->execute()) {
+            echo "Failed to execute query. <br/> $insupd_query <br/>";
+            trigger_error($stmt->error, E_USER_ERROR);
+        }
+        else {
+            echo "Successfully placed order! <br/>";
+        }
+    }
 }
 $conn->close();
 ?>
