ff0d384ae6a6a5e6a138cd556aee8e31debdfad0
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-09 01:29:26 -0500

    ready for sql

--- as3/q1/process.php
+++ as3/q1/process.php
@@ -3,12 +3,54 @@
 <head>
 <title>Sandwich Order Summary</title>
 </head>
+<!--
+The order table is updated as follows:
+If the user has a pending order of the same size of the same kind of sandwich, the quantity on that order is increased by one and the o_time is changed to the current time.
+Otherwise, a new tuple is inserted in the order table, representing this customer’s order of this sandwich, with the current time as the o_time and quantity equal to 1. Note that to do this, the app will need to fetch the customer's phone number from the session variable.
+-->
 <body>
 <?php
-    $selection = explode ('|', $_POST["selection"]);
-    $sname = $selection[0];
-    $size = $selection[1];
-    echo $_SESSION['phone'];
+session_start();
+$selection = explode ('|', $_POST["selection"]);
+$sname = $selection[0];
+$size = $selection[1];
+$phone = $_SESSION['phone'];
+
+$host = "localhost";
+$user = "root";
+$pass = "toor";
+$db = "SandwichDelivery";
+$conn = new mysqli($host, $user, $pass, $db);
+
+$chk_pending_query = "SELECT o_time
+                        FROM orders
+                        WHERE phone = " . $phone . 
+                        "AND sname = " . $sname . 
+                        "AND size = " . $size;
+$stmt = $conn->stmt_init();
+if(!$stmt->prepare($search_keyword_query)) {
+    echo "Failed to prepare. <br/>";
+}
+else {
+    $stmt->execute();
+    $stmt->bind_result($o_time);
+    $o_time_res = array();
+    $len = 0;
+    while($stmt->fetch()) {
+        $o_time_res[] = $o_time;
+        $len = $len + 1;
+    }
+    if ($len > 1) {
+        echo "Too many results. <br/>";
+    }
+    elseif ($len == 0) {
+        //need to insert new tuple
+    }
+    else {
+        //need to update existing tuple. primary key: $phone+$sname+$size+$o_time
+    }
+}
+$conn->close();
 ?>
 </body>
 </html>
