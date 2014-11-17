5fd3b652178796a80ac9a52a377e92389f4a4541
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 13:41:45 -0500

    q4b

--- as3/q4/b.sql
+++ as3/q4/b.sql
@@ -1,3 +1,6 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Question: b) Build a view CustView that lists only the cid and cname of each customer, but not the other attributes of that table. Afterwards, try to insert a new tuple into this view. Can you do it? Explain why.*/
-
+DROP VIEW IF EXISTS CustView;
+CREATE VIEW CustView AS
+    SELECT customer.cid, customer.cname FROM customer
+;
