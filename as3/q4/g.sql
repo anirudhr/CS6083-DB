c92c75009a830bae6d43e2e36c3b8320e8300790
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 14:43:08 -0500

    bleh

--- as3/q4/g.sql
+++ as3/q4/g.sql
@@ -1,6 +1,7 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
-/*Question: g) Write a query that lists any tables that contain an attribute with a name that has “name” as a substring.*/
-
+/*Question: g) Write a query that lists any tables that contain an attribute with a name that has "name" as a substring.*/
+USE videostore;
 DROP VIEW IF EXISTS CustView;
 CREATE VIEW CustView AS
     SELECT customer.cid, customer.cname FROM customer
+
