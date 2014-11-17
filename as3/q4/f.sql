805f1da0f0cdffef868a750019478daefeee631d
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 14:53:13 -0500

    q4f only constraints, not actual attributes

--- as3/q4/f.sql
+++ as3/q4/f.sql
@@ -1,6 +1,4 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Question: f) Write a query that lists all foreign keys in this database*/
 
-DROP VIEW IF EXISTS CustView;
-CREATE VIEW CustView AS
-    SELECT customer.cid, customer.cname FROM customer
+SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONSTRAINT_SCHEMA = 'videostore';
