11d0dd94e4b43cec52b5b73b9238f9848c64d8c8
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 14:56:43 -0500

    q4f

--- as3/q4/f.sql
+++ as3/q4/f.sql
@@ -1,4 +1,7 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Question: f) Write a query that lists all foreign keys in this database*/
 
-SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONSTRAINT_SCHEMA = 'videostore';
+/*SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONSTRAINT_SCHEMA = 'videostore';*/
+SELECT table_name, column_name, referenced_table_name, referenced_column_name FROM information_schema.key_column_usage
+WHERE referenced_table_name IS NOT NULL AND constraint_schema = 'videostore';
+
