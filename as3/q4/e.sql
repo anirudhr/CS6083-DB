426738dea69fb0bc1ca0ec3a5cac35e222cc7b1b
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 15:12:09 -0500

    q4e

--- as3/q4/e.sql
+++ as3/q4/e.sql
@@ -1,6 +1,5 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Question: e) Write a query that lists the number of tables and the total number of attributes of this database.*/
-
-DROP VIEW IF EXISTS CustView;
-CREATE VIEW CustView AS
-    SELECT customer.cid, customer.cname FROM customer
+USE information_schema;
+SELECT COUNT(*) AS num_tables FROM information_schema.tables
+WHERE table_schema = 'videostore';
--- as3/q4/f.sql
+++ as3/q4/f.sql
@@ -2,6 +2,7 @@
 /*Question: f) Write a query that lists all foreign keys in this database*/
 
 /*SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONSTRAINT_SCHEMA = 'videostore';*/
-SELECT table_name, column_name, referenced_table_name, referenced_column_name FROM information_schema.key_column_usage
+USE information_schema;
+SELECT table_name, column_name, referenced_table_name, referenced_column_name FROM key_column_usage
 WHERE referenced_table_name IS NOT NULL AND constraint_schema = 'videostore';
 
