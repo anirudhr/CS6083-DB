a710b327d852946e57ca41d81f8ce4da7370a79e
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 15:36:22 -0500

    q4e

--- as3/q4/e.sql
+++ as3/q4/e.sql
@@ -1,5 +1,6 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Question: e) Write a query that lists the number of tables and the total number of attributes of this database.*/
 USE information_schema;
-SELECT COUNT(*) AS num_tables FROM information_schema.tables
-WHERE table_schema = 'videostore';
+SELECT COUNT(DISTINCT tables.table_name) AS num_tables, COUNT(columns.column_name) AS num_attribs
+FROM tables JOIN columns ON tables.table_schema = columns.table_schema AND tables.table_name=columns.table_name
+WHERE tables.table_schema = 'videostore';
