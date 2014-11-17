a84f2f7455092d44d4c87c3bfa2bb5ccaf3a0cd8
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 15:39:46 -0500

    q4g

--- as3/q4/g.sql
+++ as3/q4/g.sql
@@ -1,7 +1,5 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
 /*Question: g) Write a query that lists any tables that contain an attribute with a name that has "name" as a substring.*/
-USE videostore;
-DROP VIEW IF EXISTS CustView;
-CREATE VIEW CustView AS
-    SELECT customer.cid, customer.cname FROM customer
-
+USE information_schema;
+SELECT tables.table_name FROM tables JOIN columns ON tables.table_schema = columns.table_schema AND tables.table_name=columns.table_name
+WHERE tables.table_schema = 'videostore' AND columns.column_name LIKE '%name%';
