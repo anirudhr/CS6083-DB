b417e0225803093d94623a9cc195963b5988b45a
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-11-17 19:50:43 -0500

    q4d started

--- as3/q4/d.sql
+++ as3/q4/d.sql
@@ -1,6 +1,9 @@
 /*:indentSize=4:tabSize=4:noTabs=true:wrap=soft:*/
-/*Question: d) Update the database as follows: in each branch and for each movie, if the copies of this movie were rented out more than 90% of the time on average in this branch, add one additional copy of this movie to the database for this branch.*/
+/*Question: d) Update the database as follows: in each branch and for each movie, if the copies of this movie were rented out more than 90% of the time on average in this branch, add one additional copy of this movie to the database for this branch.
+There are two ways to interpret "rented out 90% of the time":
+-- that 90% of the time, all copies were rented out (so that if someone came into the branch they would not find the copy), or
+-- that the copies were on average rented out 90% of the time (so that for 10 copies, and 30 days, the copies were rented out at least 290 of the 300 available rental days).
+You may choose either of these two interpretations. Please state which one.
+You may assume that all copies in the database already existed 30 days ago. This is because the database does not keep track of then a copy was added to the branch.*/
+USE videostore;
 
-DROP VIEW IF EXISTS CustView;
-CREATE VIEW CustView AS
-    SELECT customer.cid, customer.cname FROM customer
